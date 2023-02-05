97c7bd66c2431475ee260f7258ff581c4512578b
Ahmed Tarek <103889726+AhmedTarekAbdElRahman@users.noreply.github.com>
2023-02-05 23:53:30 +0200

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -211,7 +211,7 @@ class _LoginScreenState extends State<LoginScreen> {
           child: TextButton(
             style: TextButton.styleFrom(
               shape: const StadiumBorder(),
-              backgroundColor: Colors.blueAccent,
+              backgroundColor: Colors.blue,
               padding: const EdgeInsets.symmetric(vertical: 14),
             ),
             onPressed: () {
